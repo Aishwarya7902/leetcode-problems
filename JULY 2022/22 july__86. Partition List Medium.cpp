9cc8e4f80655409f34573abfc4027c5dff11f3ee
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-07-22 13:25:59 +0530

    Update 22 july__86. Partition List Medium.cpp

--- JULY 2022/22 july__86. Partition List Medium.cpp
+++ JULY 2022/22 july__86. Partition List Medium.cpp
@@ -4,6 +4,25 @@ Memory Usage: 10.1 MB, less than 94.54% of C++ online submissions for Partition 
 
 
 VISIT :https://leetcode.com/problems/partition-list/discuss/29185/Very-concise-one-pass-solution (DRY RUN KARO SAB CLEAR HO JAYEGA😉)
+
+PSEUDO CODE: 
+1.create two empty  node as node1 and node2
+2.pointer p1 points to node1 and pointer p2 points to p2
+3.while head is not null traverse the linked list
+from here we are in while loop
+4.if head ki value is less than x point p1->next to head and then move p1 to head
+5.if head ki value is greater  than oor equal to x point p2->next to head and then move p2 to head
+6.head=head->next
+now we are out of while loop
+7.p2->next==null coz p2 always connects nodes which are greater than or equal to x...hence at last p2 will point to the greatest no of linked list...hence 
+there will be no node after that...hence its next will be null.
+8.node2->next points to the first node which is greater than equal to x
+after while loop p1 will be at the last node which is less than x..hence all nodes after that will be greater than or equal to x.
+hence 
+p1->next=node2->next
+9.node1->next points to the first node which is less than x...hence return it....coz it will act as head.
+
+
 TC:O(N)
 SC:O(1)
 */
