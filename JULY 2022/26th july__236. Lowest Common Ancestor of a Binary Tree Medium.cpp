3925c7a2e0a09a3f4ebf11c94d1f57ad4dca174c
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-07-26 13:37:42 +0530

    Update 26th july__236. Lowest Common Ancestor of a Binary Tree Medium.cpp

--- JULY 2022/26th july__236. Lowest Common Ancestor of a Binary Tree Medium.cpp
+++ JULY 2022/26th july__236. Lowest Common Ancestor of a Binary Tree Medium.cpp
@@ -1,10 +1,10 @@
 problem:https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 blog:https://www.linkedin.com/posts/mayank-singh-1004981a4_lca-in-a-binary-tree-activity-6957524275587092480-HuLG?utm_source=linkedin_share&utm_medium=member_desktop_web
-
+best explaination (STRIVER BHAIYA): https://www.youtube.com/watch?v=_-QHfMDde90
 /*
 brute force:
 tc:O(n)
-sc:O(1)
+sc:O(n) auxiliary space for recursion
 */
 
 class Solution {
