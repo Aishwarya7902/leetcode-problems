b83520fd775a8c6e5a0d205ab62fd8214874cd0d
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-07-27 13:31:27 +0530

    Update 27th july__114. Flatten Binary Tree to Linked List Medium.cpp

--- JULY 2022/27th july__114. Flatten Binary Tree to Linked List Medium.cpp
+++ JULY 2022/27th july__114. Flatten Binary Tree to Linked List Medium.cpp
@@ -20,3 +20,30 @@ public:
         prev=root;
     }
 };
+
+/*APPROACH 2 (iterative.....dunno why it is giving tle)
+tc:O(n)
+sc:O(n) recursive call stack
+*/
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+      stack<TreeNode*>s;
+      s.push(root);
+      while(!s.empty()){
+          TreeNode*curr=s.top();
+          s.pop();
+          if(root->right){
+              s.push(root->right);
+          }
+          if(root->left){
+              s.push(root->left);
+          }
+          if(!s.empty()){
+              curr->right=s.top();
+              curr->left=NULL;
+          }
+      }
+    }
+};
