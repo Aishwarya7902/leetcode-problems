093be9a84ee6ce65116bca0ff0a3cf9ba14551e6
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-08-11 14:03:05 +0530

    Update 98. Validate Binary Search Tree.cpp

--- AUGUST 2022/98. Validate Binary Search Tree.cpp
+++ AUGUST 2022/98. Validate Binary Search Tree.cpp
@@ -18,3 +18,26 @@ public:
     }
 };
   
+/*METHOD 2: FASTER
+TC: O(N)
+SC: O(N)
+*/
+
+class Solution {
+public:
+    vector<int> res; // to store inorder order traversal
+    void solve(TreeNode* root) {
+        if(root == NULL) return; // Base Condition
+        solve(root->left); // Left subtree
+        res.push_back(root->val); // Keep pushing elements in in-order manner
+        solve(root->right); // Right subtree
+    }
+    bool isValidBST(TreeNode* root) {   
+        solve(root); // fill the vector in in-order manner
+        for(int i=1; i<res.size(); i++) {
+            if(res[i] <= res[i-1]) // If this occurs, BST condition violates
+                return false; // Not a valid BST
+        }
+        return true; // All fine if u reached here, BST is valid
+    }
+};
