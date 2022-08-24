c9c9ab3a8d8357c6735a9ed3a4f6b658767c855f
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-08-24 15:29:25 +0530

    Update 24th Aug__Power of Three.cpp

--- AUGUST 2022/24th Aug__Power of Three.cpp
+++ AUGUST 2022/24th Aug__Power of Three.cpp
@@ -72,12 +72,14 @@ INTEGER LIMITATION
 tc:O(1)
 sc:O(1)
 video:https://www.youtube.com/watch?v=UncqP2F4t_0
+PSEUDO CODE:The positive divisors of 319 are exactly the powers of 3 from 30 to 319. That's all powers of 3 in the possible range here (signed 32-bit integer).
+So just check whether the number is positive and whether it divides 319.
 */
 
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-    return  n>0 && 1162261467%n==0   ;
+   return  n>0 && int(pow(3,19))%n==0 ;
     }
 };
 
