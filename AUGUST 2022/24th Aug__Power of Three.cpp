c2738e72af6d9554ad284351cc2dedaebade90a6
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-08-24 14:32:43 +0530

    Update 24th Aug__Power of Three.cpp

--- AUGUST 2022/24th Aug__Power of Three.cpp
+++ AUGUST 2022/24th Aug__Power of Three.cpp
@@ -1,7 +1,7 @@
 PROBLEM: https://leetcode.com/problems/power-of-three/ 
 
 /*APPROACH 1
-USING LOOPS
+LOOP ITERATION
 TC: O(log3(N))
 SC: O(1)
 VIDEO:  https://www.youtube.com/watch?v=GNb8vSyw-WE
@@ -10,11 +10,28 @@ VIDEO:  https://www.youtube.com/watch?v=GNb8vSyw-WE
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        while(n>=3){
-            if(n%3!=0)return false;
+        if(n<1)  //edge case
+            return false;
+        while(n%3==0){
+           
             n/=3;
         }
         return n==1;
+    }
+};
+
+/*approach 2
+recursive:
+tc: O(log3(N))
+sc:O(log3(N))
+*/
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n<=1)  //edge case
+            return n==1;
+        return (n%3==0) && isPowerOfThree(n/3);
     }
 };
 
