bb1e8239f71ddb8ae89d20ed94f23f1f72a05c2b
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-08-31 01:48:09 +0530

    Update 30th Aug__48. Rotate Image Medium.cpp

--- AUGUST 2022/30th Aug__48. Rotate Image Medium.cpp
+++ AUGUST 2022/30th Aug__48. Rotate Image Medium.cpp
@@ -59,3 +59,25 @@ public:
         }
     }
 };
+
+/*better
+tc: 
+sc:
+video:https://www.youtube.com/watch?v=Y72QeX0Efxw
+*/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        
+      for(int i=0;i<n;i++){
+          for(int j=0;j<i;j++){
+              swap(matrix[i][j],matrix[j][i]);
+          }
+      } 
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+}; 
