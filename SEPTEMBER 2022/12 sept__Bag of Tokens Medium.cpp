98bdaf58f3921e2240ee59564ad85960bc605e78
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-09-12 02:17:04 -0700

    Create 12 sept__Bag of Tokens Medium.cpp

---
+++ SEPTEMBER 2022/12 sept__Bag of Tokens Medium.cpp
@@ -0,0 +1,37 @@
+PROBLEM:https: https://leetcode.com/problems/bag-of-tokens/
+
+/*MY INITIAL APPROACH (SOLVED ON MY OWN)
+TWO POINTER
+VIDEO:https://www.youtube.com/watch?v=JAfE7TBdTeU
+//tc:o(nlogn) sorting +o(n)
+//sc:o(1)
+*/
+
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+       int n=tokens.size();
+        if(n==1){
+            if(power<tokens[0])return 0;
+        }
+       sort(tokens.begin(),tokens.end());
+       int i=0,j=n-1,score=0,ans=0;
+        while(i<=j){
+            if(power>=tokens[i]){
+                score++;
+                power-=tokens[i];
+                i++;
+            }
+            else if(score>=1){
+                power+=tokens[j];
+                j--;
+                score--;
+            }
+            else{
+                break;
+            }
+            ans=max(ans,score);
+        }
+        return ans;
+    }
+};
