fdbe97a04e293701bf3b17bfe4d741df9c35fda2
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-09-22 06:47:14 -0700

    Create 22 september__Reverse Words in a String III Easy.cpp

---
+++ SEPTEMBER 2022/22 september__Reverse Words in a String III Easy.cpp
@@ -0,0 +1,41 @@
+PROBLEM: https://leetcode.com/problems/reverse-words-in-a-string-iii/
+/*my initial apprach
+peseudo code:
+1.take two pointer i,j and start traversing ...till u encounter a white space...move j only in this case
+2.as soon as u encounter a white space ....then the index just before it will be the last character of the previous word
+3.set k=j+2 ...as the index one after j is a white space so we set k=j+2
+4.now reverse the substring between i and j pointers
+5.follow above steps untill u have reversed every word of string
+tc: O(n*m)
+here n is the length of string and m is the length of largest word in the string
+sc: O(1) as we did not take any extra space
+
+video:
+
+*/
+class Solution {
+public:
+  void Reverse(string &s,int i,int j){
+    while(i<=j){
+      char temp=s[i];
+      s[i]=s[j];
+      s[j]=temp;
+      i++;
+      j--;
+    }
+  }
+    string reverseWords(string s) {
+     int n=s.size();
+      int i=0,j=0,k;
+      while(i<n){
+        while(s[j+1]!=' '  && j+1<n){
+          j++;
+        }
+        k=j+2;
+        Reverse(s,i,j);
+        i=k;
+        j=k;
+      }
+      return s;
+    }
+};
